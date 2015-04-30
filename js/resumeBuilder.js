278edbcacbee5f2a9ac5d1e05149866fafd6f245
Laura Curley <laura@incling.com.au>
2015-04-30 17:12:31 +1000

    sync

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,13 +6,111 @@ var role = "Developer";
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
 var bio = {
-	"name":"Laura Curley",
-	"role":"Developer",
-	"contact info":"0411101454",
-	"skills":"some skills"
-};
+	"name" : "Laura Curley",
+	"role" : "Developer",
+	"contacts" : {
+		"mobile" : "0411 101 454",
+		"email" : "laura@incling.com.au",
+		"location" : "Bangalow"
+	},
+	"welcomeMessage" : "lorem ipsum dolor sit amet etc etc.",
+	"skills" : [
+		"awesomeness", "delivering things", "cryogenic sleeping", "saving the universe"
+	],
+	"bioPic" : "images/fry.jpg"
+}
+
+var work = {
+	"jobs" : [
+		{
+			"employer" : "",
+			"title" : "",
+			"location" : "",
+			"dates" : "",
+			"description" : ""
+		},
+		{
+			"employer" : "",
+			"title" : "",
+			"location" : "",
+			"dates" : "",
+			"description" : ""
+		}
+	]	
+}
+
+var projects = {
+	"projects" : [
+		{
+			"title" : "",
+			"dates" : "",
+			"description" : "",
+			"images" : [
+				{
+					"image1" : "url",
+					"image2" : "url"
+				}
+			]
+		},
+		{
+			"title" : "",
+			"dates" : "",
+			"description" : "",
+			"images" : [
+				{
+					"image1" : "url",
+					"image2" : "url"
+				}
+			]
+		}
+	]
+}
+
+var education = {
+	"schools" : [
+		{
+			"name" : "University of North Carolina",
+			"degree" : [
+				{
+					"first" : "Master of Landscape Architecture"
+				}
+			],
+			"location" : "Raleigh, North Carolina, USA",
+			"dates" : "",
+			"url" : "www.unc.edu"
+		},
+		{
+			"name" : "University of North Carolina",
+			"degree" : [
+				{
+					"first" : "Master of Landscape Architecture"
+				}
+			],
+			"location" : "Raleigh, North Carolina, USA",
+			"dates" : "",
+			"url" : "www.unc.edu"
+		}
+	],
+	"onlineCourse" : [
+		{
+			"title": "",
+			"school" : "",
+			"dates" : "",
+			"url" : ""
+		},
+		{
+			"title": "",
+			"school" : "",
+			"dates" : "",
+			"url" : ""
+		}
+	]
+}
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
-$("#header").prepend(bio);
+$("#header").append(bio.name);
+$("#header").append(education.degree);
+
+
 
